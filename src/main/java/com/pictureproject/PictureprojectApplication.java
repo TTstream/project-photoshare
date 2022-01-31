60280934f31e6f7f308d049765db8004eb918e52
SeongJin Oh <guk0230@naver.com>
2022-01-31 20:28:27 +0900

    init

---
+++ src/main/java/com/pictureproject/PictureprojectApplication.java
@@ -0,0 +1,13 @@
+package com.pictureproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictureprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PictureprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pictureproject/PictureprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pictureproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PictureprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
