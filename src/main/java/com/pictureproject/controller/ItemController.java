dd9678d656e6a7f6e577226a4f8496c3c97ba34c
SeongJin Oh <guk0230@naver.com>
2022-02-12 17:41:22 +0900

    Feat: ItemController(물품 조회 api 추가)

--- src/main/java/com/pictureproject/controller/ItemController.java
+++ src/main/java/com/pictureproject/controller/ItemController.java
@@ -6,12 +6,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 import java.util.List;
 
@@ -49,5 +47,24 @@ public class ItemController {
 
         return "redirect:/";
     }
+
+    //물품 수정시 물품 조회
+    @GetMapping(value = "/user/item/{itemId}")
+    public String itemDtl(@PathVariable("itemId") Long itemId,Model model){
+
+        try{
+            ItemFormDto itemFormDto=itemService.getItemDtl(itemId);
+            model.addAttribute("itemFormDto",itemFormDto);
+        }catch (EntityNotFoundException e){
+            model.addAttribute("errorMessage","존재하지 않는 물품 입니다.");
+            model.addAttribute("itemFormDto",new ItemFormDto());
+            return "item/itemForm";
+        }
+
+        return "item/itemForm";
+
+    }
+
+
 
 }
