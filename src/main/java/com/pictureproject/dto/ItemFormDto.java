ee46b412bef86d1113c940f03f032fefe8a3d710
SeongJin Oh <guk0230@naver.com>
2022-02-12 10:38:38 +0900

    Feat: ItemFormDto.java 추가

---
+++ src/main/java/com/pictureproject/dto/ItemFormDto.java
@@ -0,0 +1,41 @@
+package com.pictureproject.dto;
+
+import com.pictureproject.entity.Item;
+import lombok.Getter;
+import lombok.Setter;
+import org.modelmapper.ModelMapper;
+
+import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter @Setter
+public class ItemFormDto {
+
+    private Long id;
+
+    @NotBlank(message = "제목은 필수 입력 값입니다.")
+    private String title; //제목
+
+    @NotBlank(message = "상세 설명은 필수 입력 값입니다.")
+    private String itemDetail; //물품 상세 설명
+
+    //물품 수정할 때 물품 이미지 정보를 저장하는 리스트
+    private List<ItemImgDto> itemImgDtoList=new ArrayList<>();
+
+    //물품 수정할 때 물품의 이미지 아이디를 저장하는 리스트
+    private List<Long> itemImgIds=new ArrayList<>();
+
+    private static ModelMapper modelMapper=new ModelMapper();
+
+    //물품 생성시 ItemFormDto를 Item으로 변환후 값 저장
+    public Item createItem(){
+        return modelMapper.map(this,Item.class);
+    }
+
+    //물품 조회시 Item을 ItemFormDto로 변환 후 값 저장
+    public static ItemFormDto of(Item item){
+        return modelMapper.map(item,ItemFormDto.class);
+    }
+
+}
